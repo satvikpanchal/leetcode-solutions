d051201626f1ccf26a6aa6e0e321a8bac5c8644a
satvikpanchal <satvikpanchal@users.noreply.github.com>
2025-10-12 01:51:47 +0000

    Sync LeetCode solutions   Organized by language/difficulty at repo root.   Timestamp: 2025-10-12 01:51:47 UTC

---
+++ python3/easy/1514.minimum-value-to-get-positive-step-by-step-sum.py
@@ -0,0 +1,24 @@
+# LeetCode Solution
+# Problem: 1514. Minimum Value to Get Positive Step by Step Sum
+# Difficulty: Easy
+# Language: Python3
+# URL: https://leetcode.com/problems/minimum-value-to-get-positive-step-by-step-sum/
+# Submitted: 2025-10-12 00:21:41 UTC
+# Status: Accepted
+
+class Solution:
+    def minStartValue(self, nums: List[int]) -> int:
+
+        min_start = 1
+        total_sum = 1
+
+        for i in nums:
+            # [-3,2,-3,4,2]
+            total_sum += i  # -3 + -1 = -4
+            # total_sum += min_start
+
+            if total_sum <= 0:
+                min_start += abs(total_sum) + 1 # 4
+                total_sum = 1
+
+        return min_start
