c945cc1b5d862fb2b45fb5ad206ca0631259309b
satvikpanchal <satvikpanchal@users.noreply.github.com>
2026-05-06 14:21:21 +0000

    Sync LeetCode solutions   Organized by language/difficulty at repo root.   Timestamp: 2026-05-06 14:21:21 UTC

--- python3/easy/792.binary-search.py
+++ python3/easy/792.binary-search.py
@@ -3,7 +3,7 @@
 # Difficulty: Easy
 # Language: Python3
 # URL: https://leetcode.com/problems/binary-search/
-# Submitted: 2026-04-27 23:07:48 UTC
+# Submitted: 2025-07-05 02:26:26 UTC
 # Status: Accepted
 
 class Solution:
@@ -11,13 +11,14 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = (l + r) // 2
+            midpoint = (l + r) // 2
 
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                l = mid + 1
+            if nums[midpoint] > target:
+                r = midpoint - 1
+            elif nums[midpoint] < target:
+                l = midpoint + 1
             else:
-                r = mid - 1
+                return midpoint
 
         return -1
+        
